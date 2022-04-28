daeadd838d7b50a6f4d4fa3a447b0695560fe6fd
FlutLab.io <>
2022-04-28 20:22:33 +0000

    Autosave

--- lib/Screens/Boton_Flotante_screen.dart
+++ lib/Screens/Boton_Flotante_screen.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
- class Boton_flotante extends StatelessWidget {
+class Boton_flotante extends StatelessWidget {
   Boton_flotante({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("Pantalla de boton"))),
+      appBar: AppBar(title: Center(child: Text("Boton flotante"))),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hola mundo"),
-          ],
-        )
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Hola mundo"),
+        ],
+      )),
     );
   }
 }
