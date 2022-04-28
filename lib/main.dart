9e4e968e30e5af65410b7248df6bf91364e7530a
FlutLab.io <>
2022-04-28 20:18:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Boton_flotante(),
-      theme: ThemeData(primarySwatch: Colors.orange),
+      theme: ThemeData(primarySwatch: Colors.red),
     );
   }
 }
